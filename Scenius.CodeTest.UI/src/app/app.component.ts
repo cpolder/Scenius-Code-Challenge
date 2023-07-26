f7b2d9851805e99b5bb02f96ec40ae6225abd467
Nick <nick.heskes@scenius.nl>
2023-07-26 16:07:03 +0200

    Newer version, have display of ready calculations done

--- Scenius.CodeTest.UI/src/app/app.component.ts
+++ Scenius.CodeTest.UI/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { HttpClient } from '@angular/common/http';
+import { CalculationComponent } from './calculation/calculation.component';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent {
   title = 'scenius-codetest';
+  hallo : Calculation[];
+
+  constructor(private _http : HttpClient) {
+    this.hallo = [];
+  }
+
+  function() {
+    this._http.get('http://localhost:5000/ingest').subscribe(result => {
+      this.hallo = <Array<Calculation>>result;
+    });
+  }
+}
+export interface Calculation {
+  id: Number;
+  input: string;
+  result: Number;
 }
---
+++ Scenius.CodeTest.UI/src/app/calculation/calculation.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculation',
+  templateUrl: './calculation.component.html',
+  styleUrls: ['./calculation.component.scss']
+})
+export class CalculationComponent {
+  input: String;
+  result: Number;
+
+  constructor(input: String, result: Number) {
+    this.input = input;
+    this.result = result;
+  }
+
+  function
+}
